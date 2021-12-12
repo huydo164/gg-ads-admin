67c38733b91b8ca82842a4f65bbc020005e6ab4b
huy <huy.dvn@digidinos.com>
2021-12-12 11:04:26 +0700

    google-ads

---
+++ components/organisms/adsense/AdsenseDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="adsense-detail">
+    <span style="font-size: 40px">This for Advertisement</span>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  data () {
+    return {
+      data: [],
+    }
+  },
+
+  mounted () {
+    this.fetch()
+  },
+
+  methods: {
+    async fetch () {
+      try {
+        const { data } = await axios.get('http://127.0.0.1:8000/api/article')
+        this.data = data.data
+
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.adsense-detail {
+  width: 100%;
+  height: auto;
+  text-align: center;
+  line-height: auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="article-adsenses margin-bottom-20">
+    <AdsenseDetail />
+  </div>
+</template>
+
+<script>
+import AdsenseDetail from "~/components/organisms/adsense/AdsenseDetail"
+export default {
+  components: {
+    AdsenseDetail
+  }
+}
+</script>
